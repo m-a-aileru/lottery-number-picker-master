96d499397d6a3979ad67ba4e669d72cd55b7ebe7
m-a-aileru <m.a.aileru@gmail.com>
2023-04-01 21:42:42 +0100

    Test commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lottery-number-picker
+A prototype for an online web app that helps you pick your lottery numbers (if ever you're stuck for picking your own).
---
+++ lnp.js
@@ -0,0 +1,48 @@
+var x = document.getElementsByTagName('p'); 
+
+// Event listeners
+document.addEventListener("keydown", eventManager);
+
+function generateRandomNumbers(a) {
+   for (var i = 0; i < a.length; i++) {
+       a[i].innerHTML = Math.round(Math.random()*48)+1;
+   }
+}
+
+function findDuplicates(b) {
+    for (var i = 0; i < b.length; i++) {
+        for (var j = i; j < b.length; j++) {
+            if (i !== j && b[i] == b[j]) {
+                b[j] = Math.ceil(Math.random()*49); /** draw a different number */
+               /** b[j] += "d";   - left in for future testing purposes */
+            } 
+        }
+    }
+}
+
+function drawOrRedraw() {
+    generateRandomNumbers(x);
+    
+    var nodeArray = [];
+    
+    // Create 'snapshot' of contents of the NodeList and put them into an array
+    for (var i = 0; i < x.length; ++i) {
+        nodeArray[i] = x[i].innerHTML;
+    }
+    
+    findDuplicates(nodeArray);
+    
+    for (var i = 0; i < nodeArray.length; ++i) {
+        x[i].innerHTML = nodeArray[i];
+    }
+    
+    return nodeArray;        
+}
+
+function eventManager(event) {
+    var x = event.key;
+    
+    if (x === "d" || x === "D") {
+        drawOrRedraw();
+    }
+}
